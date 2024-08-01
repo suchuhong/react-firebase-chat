a3c9a5983944b4a3667415a9f9a0b862388eba48
suchuhong <1442615457@qq.com>
2024-08-02 00:45:50 +0800

    增加用户列表样式，支持滚动

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,6 +1,8 @@
 import "./chatList.css";
+import { useState } from "react";
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
     <div className="chatList">
       <div className="search">
@@ -8,7 +10,47 @@ const ChatList = () => {
           <img src="./search.png" alt="" />
           <input type="text" placeholder="search" />
         </div>
-        <img src="./plus.png" alt="" className="add" />
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>susu</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>susu</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>susu</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>susu</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>susu</span>
+          <p>Hello</p>
+        </div>
       </div>
     </div>
   );
