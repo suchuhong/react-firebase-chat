3ac915a625da3a46ae2db8fb26dc15d343270a5c
suchuhong <1442615457@qq.com>
2024-08-07 01:15:05 +0800

    增加emoji组件

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,14 @@
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
+import { useState } from "react";
 
 const Chat = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleEmoji = (e) => {
+    console.log(e);
+  };
+
   return (
     <div className="chat">
       <div className="top">
@@ -26,7 +34,12 @@ const Chat = () => {
         </div>
         <input type="text" placeholder="Type a message..." />
         <div className="emoji">
-          <img src="./emoji.png" alt="" />
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <EmojiPicker open={open} onEmojiClick={handleEmoji} />
         </div>
         <button className="sendButton">send</button>
       </div>
